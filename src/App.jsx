1399f026b10e46046282b577879dbc1f8b898b07
mariana3299 <mariana.ag3299@gmail.com>
2024-08-15 18:53:40 -0600

    feat: subiendo examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import './App.css'
-import Card from './Components/Card'
+import { useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
+  const [form, setForm] = useState({
+    name: "",
+    game: ""
+  })
+
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChangeName = (e) => setForm({...form, name:e.target.value});
+  const handleChangeGame = (e) => setForm({...form, game:e.target.value});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const {name,game} = form;
+
+    if(name.trim().length>3 && !(name.trimEnd().includes(" ")) && game.trim().length>=6){
+      setShow(true);
+      setError(false);
+    }else{
+      setError(true);
+      setShow(false)
+    }
+  }
+
 
   return (
     <>
-    <h1>Carga de estudiantes</h1>
-    <form></form>
-      <Card />
+      <h1>Carga de estudiantes</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Nombre</label>
+        <input type="text" onChange={handleChangeName} value={form.name} />
+        <label>Videojuego favorito</label>
+        <input type="text" onChange={handleChangeGame} value={form.game} />
+        <button>Enviar</button>
+      </form>
+      {show && (<Card name={form.name} game={form.game} />)}
+      {error && (<h4 style={{color:"red"}}>Por favor chequea que la información sea correcta</h4>)}
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,10 @@
-const Card = () => {
+const Card = ({name, game}) => {
   return (
-    <div>Card</div>
+    <div className="cardContainer">
+      <h4>Hola {name}!</h4>
+      <h4>Sabmeos que tu videojuego favorito es: </h4>
+      <h3>{game}</h3>
+    </div>
   )
 }
 
